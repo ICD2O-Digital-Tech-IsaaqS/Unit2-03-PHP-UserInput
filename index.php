3b05d3b127df638f23adc8d7604e5295231f57d9
Isaaq Simon <IsaaqS>
2025-03-26 14:53:40 +0000

    Street PHP

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
     <!-- Metadata -->
     <meta charset="utf-8">
     <meta name="description" content="My first webpage with user input">
-    <meta name="keywords" content="immaculata, ics2o">
-    <meta name="author" content="Ms Raffin">
+    <meta name="keywords" content="Immaculata, ICD2O">
+    <meta name="author" content="Isaaq Simon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Code for the favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png">
-    <link rel="manifest" href="./favicon/site.webmanifest">
+    <link rel="apple-touch-icon" sizes="180x180" href="./favicon_io (4)/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./favicon_io (4)/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./favicon_io (4)/favicon-16x16.png">
+    <link rel="manifest" href="./favicon_ (4)/site.webmanifest">
     <!-- Link to CSS file -->
     <link rel="stylesheet" href="./css/style.css">
     <!-- Title -->
@@ -20,17 +20,17 @@
   <body>
     <?php echo "<h1>User Input in PHP</h1>" ?>
     <form action="./welcome.php" method="post" target="result">
-      <label for="first-name">First Name</label>
-      <input type="text" id="first-name" placeholder="Your Name" name="first-name"><br><br>
-      <label for="age">Age</label>
-      <input type="text" id="age" placeholder="Your Age" name="age"><br><br>
+      <label for="street-number">Street Number</label>
+      <input type="number" id="street-number" placeholder="Street Number" name="street-number"><br><br>
+      <label for="street-name">Street Name</label>
+      <input type="text" id="street-name" placeholder="Street Name" name="street-name"><br><br>
       <input type="submit" value="Enter">
 			<br><br>	
     </form>
 <div id="result"></div>
     <center>
       <iframe id="result" name="result">
-				Your first name is <?php echo $_POST["first-name"]; ?> and your age is <?php echo $_POST["age"]; ?>.
+				Your Street Number is <?php echo $_POST["street-number"]; ?> and your Street Name is <?php echo $_POST["street-name"]; ?>.
       </iframe>
     </center>
   </body>
